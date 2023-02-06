89c738c58124d0861d45379377a605a731d77486
DonBahall <dimachar2004@gmail.com>
2023-02-06 16:29:12 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,109 +1,148 @@
-import './App.css'
 import React from "react";
-import Button from '@material-ui/core/Button';
-import {List, ListItem, ListItemText, TextField} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import {List, ListItem, ListItemText} from "@material-ui/core";
+import './App.css'
 
+let history = [];
 
-const history = [];
+class App extends React.Component {
 
-function App() {
-    let i = 0;
-    const [input, setInput] = React.useState("");
-    const nums = Array();
-    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "."].forEach((item) => {
-        nums.push(
-            <Button
-                onClick={(e) => {
-                    setInput(input + e.currentTarget.value);
-                }}
-                value={item}
-                key={item}
-            >
-                {" "}
-                {item}
-            </Button>
-        );
-    });
-    return (
+    tape(value) {
+        let curVal = value;
+        let output = this.refOut.current
+        this.setState({
+            out: curVal
+        })
+        if (output.value === '0') {
+            output.value = ''
+        }
+        output.value += curVal;
+    }
+    tapeOper(value){
+        let curVal = value;
+        let output = this.refOut.current
+        let op = ['+','-','*','/'];
+        this.setState({
+            out: curVal
+        })
+        let lastElement = output.value.substring(output.value.length - 1);
+        for (let i = 0; i < op.length;i++)
+            if (lastElement === op[i]) {
+                output.value = output.value.substring(0, output.value.length - 1)
+            }
 
-        <div className="wrapper">
-            {" "}
-            <div className="screen">{input}</div>
-            <div className="button">{nums}</div>
-            <div className="subgrid">
+        output.value += curVal;
+    }
 
-                <Button onClick={() => setInput(input.substr(0, input.length - 1))}>
-                    Clear
+    calc(input) {
+        let value = input.match((/(^[0-9*\/\\()+-]+$)/));
+        let res = new Function('return ' + value);
+        return res();
+    }
+
+    operationsWork(value) {
+        let output = this.refOut.current
+        if (value === 'AC') {
+            output.value = output.value.substring(0, output.value.length - 1)
+        } else if (value === 'Cl') {
+            output.value = ''
+        } else if (value === '=') {
+            try {
+                history.push(output.value + '=' + calc(output.value))
+                output.value = calc(output.value)
+            }catch (e){
+                console.log(e)
+            }
+        }
+    }
+
+    oper() {
+        const oper = [];
+        ['+', '-', '*', '/'].forEach((i) => {
+            oper.push(
+                <Button onClick={(e) => this.tapeOper(i)}
+                        value={i}
+                        key={i}>
+                    {i}
                 </Button>
-                <Button onClick={() => setInput("")} value="">
-                    AC
-                </Button>
-            </div>
+            )
+        });
+        return oper;
+    }
 
-            <div className="subgrid">
-
-                <Button onClick={(e) => setInput(input + e.currentTarget.value)} value={"+"}>
-                    +
-                </Button>
-
-                <Button onClick={(e) => setInput(input + e.currentTarget.value)} value="-">
-                    {" "}
-                    -
-                    {" "}
-                </Button>
-
-                <Button onClick={(e) => setInput(input + e.currentTarget.value)} value="*">
-                    {" "}
-                    *
-                </Button>
-
-                <Button onClick={(e) => setInput(input + e.currentTarget.value)} value="/">
-                    {" "}
-                    /
-                </Button>
-
-                <Button
-                    onClick={(e) => {
-                        try {
-                            let ans = String(eval(input));
-                            setInput(
-                                ans.includes(".") ?
-                                    ans.toFixed(4) : calc(input));
-                            historyPush(input + "=" + eval(input));
-                        } catch (e) {
-                            console.log(e);
-                        }
-                    }}
-                    value="="
+    numbers() {
+        const nums = [];
+        [9, 8, 7, 6, 5, 4, 3, 2, 1, 0].forEach((item) => {
+            nums.push(
+                <Button onClick={(e) => this.tape(item)}
+                        value={item}
+                        key={item}
                 >
-                    =
+                    {" "}
+                    {item}
                 </Button>
+            );
+        });
+        return nums;
+    }
+
+    constructor() {
+        super();
+        this.state = {
+            out: "0"
+        }
+        this.refOut = React.createRef();
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                {" "}
+                <div className={"screen"}>
+                    <input ref={this.refOut} type={"text"} defaultValue={this.state.out}/>
+                </div>
+                <div className="button">{this.numbers()}</div>
+                <div className="subgrid">
+                    <Button onClick={() => this.operationsWork('AC')}>
+                        AC
+                    </Button>
+                    <Button onClick={() => this.operationsWork('Cl')}>
+                        Clear
+                    </Button>
+                </div>
+                <div className="subgrid">
+                    {this.oper()}
+                    <Button onClick={() => this.operationsWork('=') }>
+                        =
+                    </Button>
+                </div>
+
+                <div className={"history"}>
+                    <Button onClick={(e) => sendToBack(history)}>Get and solve</Button>
+                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                        { history.map((value, index) => (
+                            <ListItem
+                                key={value}
+                                disableGutters
+                            >
+                                <ListItemText className={`${index === 0 ? "active" : ""}`} primary={`${value}`} />
+
+                            </ListItem>
+                        ))}
+                    </List>
+
+                </div>
             </div>
-
-            <div className={"history"}>
-                <Button onClick={(e) => sendToBack()}>Get and solve</Button>
-
-                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                    { history.map((value) => (
-
-                    <ListItem
-                        key={value}
-                        disableGutters
-                    >
-                        <ListItemText primary={`${value}`} />
-
-                    </ListItem>
-
-                    ))}
-                </List>
-
-            </div>
-        </div>
-
-    );
-
+        )
+    }
 }
-async function sendToBack() {
+function calc(input) {
+    let value = input.match((/(^[0-9*\/\\()+-]+$)/));
+    let res = new Function('return ' + value);
+    return res();
+}
+
+async function sendToBack(history) {
     return await fetch('http://localhost:8080/math/examples?count=5', {
         method: 'GET',
         mode: 'cors',
@@ -111,7 +150,7 @@ async function sendToBack() {
     }).then((response) => response.json())
         .then((json) => {
             for (let i = 0; i < json.length; i++) {
-                historyPush(json[i] + "=" + calc(json[i]))
+                history.push(json[i] + "=" + calc(json[i]))
             }
         })
         .catch((error) => {
@@ -119,14 +158,5 @@ async function sendToBack() {
         });
 }
 
-function calc(input) {
-    let value = input.match((/(^[0-9*\/\\()+-]+$)/));
-    let res = new Function('return ' + value);
-    return res();
-}
 
-function historyPush(input) {
-    history.push(input);
-}
-
-export default App;
+export default App
